365d43ce4a9713142c5e4b073dc05ce4ce5d1e6f
Eugene Kusnetsov <phpsql.net@gmail.com>
2018-09-20 01:43:55 +0300

    add mycron file

--- set_cron_task.sh
+++ set_cron_task.sh
@@ -1,7 +1,7 @@
 #write out current crontab
-crontab -l > mycron
+crontab -l > /data/test/mycron
 #echo new cron into cron file
-echo "1 * * * * python3 \data\test\restart_if_repo_changed.py" >> mycron
+echo "1 * * * * python3 \data\test\restart_if_repo_changed.py" >> /data/test/mycron
 #install new cron file
-crontab mycron
+crontab /data/test/mycron
 

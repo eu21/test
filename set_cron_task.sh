225619b12c0252f24605f86eac81ec517aec981f
Eugene Kusnetsov <phpsql.net@gmail.com>
2018-09-20 12:12:09 +0300

    Fixed EOL in comparison to not up to date\n

--- set_cron_task.sh
+++ set_cron_task.sh
@@ -1,4 +1,4 @@
-#write out current crontab
+#write out current crontab to file
 crontab -l > mycron
 #echo new cron into cron file
 echo "1 * * * * root python3 /data/test/restart_if_repo_changed.py" >> mycron

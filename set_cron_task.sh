3021a9ab2ec980dc315b3d3904dbeaf3fe69f00b
Eugene Kusnetsov <phpsql.net@gmail.com>
2018-09-20 12:56:29 +0300

    fixing minor bugs

--- set_cron_task.sh
+++ set_cron_task.sh
@@ -1,8 +1,8 @@
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-#echo "1 * * * * root python3 /data/test/restart_if_repo_changed.py" >> mycron
-echo "1 * * * * root python3 /data/test/restart_if_repo_changed.py" > mycron
+#echo "1 * * * * root python3 /data/test/restart_if_repo_changed.py" > mycron
+echo "* * * * * /usr/bin/python3 /data/test/restart_if_repo_changed.py" >> mycron
 #install new cron file
 crontab mycron
 

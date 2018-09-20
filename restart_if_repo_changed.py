6631add150faa82c774d9f38a93e5fa9c70887ac
Eugene Kusnetsov <phpsql.net@gmail.com>
2018-09-20 08:24:12 +0300

    Fixed EOL in comparison to not up to date\n

--- restart_if_repo_changed.py
+++ restart_if_repo_changed.py
@@ -22,7 +22,7 @@ def run_bash_command(command):
 up_to_date = run_bash_command("cd /data/test && [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @\{u\} | \
 sed 's/\// /g') | cut -f1) ] && echo up to date || echo not up to date")
 
-if up_to_date=="not up to date":
+if up_to_date=="not up to date\\n":
             run_bash_command('sudo kill $(pgrep -f \'python3 /data/test/httpsrv.py\')')
             run_bash_command('cd /data/test && sudo git pull origin master')
             run_bash_command('sudo python3 /data/test/httpsrv.py > /dev/null 2>&1 &')
